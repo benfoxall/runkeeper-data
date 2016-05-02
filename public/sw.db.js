f3db4204e7f7ea674cfb008406a44daa41ac5345
Ben Foxall <ben@pusher.com>
2016-05-02 18:18:15 +0200

    error handle on db

--- public/sw.db.js
+++ public/sw.db.js
@@ -1,8 +1,8 @@
 importScripts('components/dexie/dist/dexie.min.js')
 
-Dexie.Promise.on('error', console.error.bind(console, 'Dexie: '))
-
 var db = new Dexie('Runkeeper')
+
+db.on("error", function(e) { console.error (e.stack || e) })
 
 db.version(1).stores({
   activities: '&uri'
